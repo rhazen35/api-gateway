22ff0b7c90ce53966c75993cf7cee0f06049ce95
Ruben Hazenbosch <rh@braune-digital.com>
2021-10-07 17:36:17 +0200

    Initial Setup

---
+++ src/Messenger/Publish/PublishMessageFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/*************************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2021 Ruben Hazenbosch <rh@braune-digital.com>, Braune Digital GmbH
+ *
+ *  All rights reserved
+ ************************************************************************/
+
+declare(strict_types=1);
+
+namespace App\Messenger\Publish;
+
+class PublishMessageFactory
+{
+
+}
